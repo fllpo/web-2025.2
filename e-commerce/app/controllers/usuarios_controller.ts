43c22caa66d56d8905abde8cd9437c66a1d60521
Èlyda Rodrigues de Oliveira <elydarodrigues66@gmail.com>
2025-11-03 20:23:12 -0300

    feat: add user menu

--- e-commerce/app/controllers/usuarios_controller.ts
+++ e-commerce/app/controllers/usuarios_controller.ts
@@ -13,6 +13,28 @@ export default class UsuariosController {
     try {
       const dados = await request.validateUsing(criarUsuarioValidator)
 
+      const imagem = request.file('imagem', {
+        size: '2mb',
+        extnames: ['jpg', 'jpeg', 'png', 'webp'],
+      })
+
+      if (imagem && !imagem.isValid)
+        return response.status(422).json({
+          status: 'error',
+          message: 'Arquivo inválido',
+          erros: imagem.errors,
+        })
+
+      let nomeImagem
+      try {
+        nomeImagem = await this.usuarioService.salvarImagem(imagem)
+      } catch (error) {
+        return response.status(500).json({
+          status: 'error',
+          message: 'Erro ao fazer upload da imagem',
+        })
+      }
+
       console.log('🔴 DADOS RECEBIDOS NO CONTROLLER:')
       console.log('Email:', dados.email)
       console.log('Senha:', dados.senha)
@@ -25,7 +47,19 @@ export default class UsuariosController {
         return response.badRequest('Usuário já cadastrado.')
       }
 
-      await this.usuarioService.criar(dados)
+      await this.usuarioService.criar({
+        nome: dados.nome,
+        telefone: dados.telefone,
+        cep: dados.cep,
+        logradouro: dados.logradouro,
+        numero: dados.numero,
+        cidade: dados.cidade,
+        estado: dados.estado,
+        email: dados.email,
+        senha: dados.senha,
+        bairro: dados.bairro,
+        fotoPerfil: nomeImagem || undefined,
+      })
       return response.redirect().toRoute('usuario.login')
     } catch (error) {
       if (error.messages) {
--- e-commerce/app/services/usuario_service.ts
+++ e-commerce/app/services/usuario_service.ts
@@ -1,11 +1,68 @@
 import Usuario from '#models/usuario'
+import app from '@adonisjs/core/services/app'
+import { cuid } from '@adonisjs/core/helpers'
+import path from 'node:path'
+import sharp from 'sharp'
 
 export class UsuarioService {
   async buscarPorEmail(email: string) {
     return Usuario.findBy('email', email)
   }
 
+  async salvarImagem(arquivo: any) {
+    if (!arquivo) {
+      return null
+    }
+
+    try {
+      const novoNome = `${cuid()}.webp`
+      const caminho = path.join(app.makePath('resources/images/uploads/usuario'), novoNome)
+
+      await sharp(arquivo.tmpPath!)
+        .resize(800, 800, {
+          fit: 'contain',
+          background: { r: 255, g: 255, b: 255, alpha: 1 },
+        })
+        .webp({ quality: 85 })
+        .toFile(caminho)
+
+      return novoNome
+    } catch (error) {
+      console.log('Erro ao salvar a imagem', error)
+      throw new Error('Erro ao fazer upload da imagem')
+    }
+  }
+
   async criar(dados: any) {
     return Usuario.create(dados)
+  }
+
+  async atualizar(
+    id: string,
+    dados: {
+      nome?: string
+      telefone?: string
+      cep?: string
+      logradouro?: string
+      numero?: number
+      cidade?: string
+      estado?: string
+      email?: string
+      senha?: string
+      bairro?: string
+      fotoPerfil?: string
+    }
+  ) {
+    // const produto = await Produto.find(id)
+    const usuario = await Usuario.find(id)
+
+    if (!usuario) {
+      return null
+    }
+
+    usuario.merge(dados)
+    await usuario.save()
+
+    return usuario
   }
 }
